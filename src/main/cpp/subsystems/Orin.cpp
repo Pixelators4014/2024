c74e1adbf251ff890ec2c5ead427858689259f3c
Ashwin Naren <arihant2math@gmail.com>
2024-03-19 14:11:12 -0700

    I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP

--- src/main/cpp/subsystems/Orin.cpp
+++ src/main/cpp/subsystems/Orin.cpp
@@ -9,63 +9,62 @@
 #include "subsystems/Orin.h"
 
 Orin::Orin() {
-  // Creating socket file descriptor
-  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-    std::cerr << "Socket creation failed" << std::endl;
-  }
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        std::cerr << "Socket creation failed" << std::endl;
+    }
 
-  memset(&servaddr, 0, sizeof(servaddr));
-  memset(&cliaddr, 0, sizeof(cliaddr));
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
 
-  // Filling server information
-  servaddr.sin_family = AF_INET;         // IPv4
-  servaddr.sin_addr.s_addr = INADDR_ANY; // localhost
-  servaddr.sin_port = htons(PORT);
+    // Filling server information
+    servaddr.sin_family = AF_INET;         // IPv4
+    servaddr.sin_addr.s_addr = INADDR_ANY; // localhost
+    servaddr.sin_port = htons(PORT);
 
-  // Bind the socket with the server address
-  if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-    std::cerr << "Bind failed" << std::endl;
-  }
+    // Bind the socket with the server address
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        std::cerr << "Bind failed" << std::endl;
+    }
 
-  std::cout << "Listening on port " << PORT << "..." << std::endl;
+    std::cout << "Listening on port " << PORT << "..." << std::endl;
 }
 
 void Orin::Periodic() {
-  int n = recvfrom(sockfd, (char *)buffer, BUFFER_SIZE, MSG_WAITALL,
-                   (struct sockaddr *)&cliaddr, &len);
-  if (n < 0) {
-    std::cerr << "Receive failed" << std::endl;
-    return;
-  }
-  buffer[n] = '\0'; // Null terminate the string
+    int n = recvfrom(sockfd, (char *)buffer, BUFFER_SIZE, MSG_WAITALL,
+                     (struct sockaddr *)&cliaddr, &len);
+    if (n < 0) {
+        std::cerr << "Receive failed" << std::endl;
+        return;
+    }
+    buffer[n] = '\0'; // Null terminate the string
 
-  if (n < 32) {
-    std::cerr << "Not enough data received" << std::endl;
-    return;
-  }
+    if (n < 32) {
+        std::cerr << "Not enough data received" << std::endl;
+        return;
+    }
 
-  float x, y, z, theta, dx, dy, dz, dtheta;
-  memcpy(&x, buffer, sizeof(float));
-  memcpy(&y, buffer + 4, sizeof(float));
-  memcpy(&z, buffer + 8, sizeof(float));
-  memcpy(&theta, buffer + 12, sizeof(float));
-  memcpy(&dx, buffer + 16, sizeof(float));
-  memcpy(&dy, buffer + 20, sizeof(float));
-  memcpy(&dz, buffer + 24, sizeof(float));
-  memcpy(&dtheta, buffer + 28, sizeof(float));
+    unsigned header;
+    float x, y, z, theta_w, theta_x, theta_y, theta_z;
+    memcpy(&header, buffer, sizeof(unsigned));
+    memcpy(&x, buffer + 4, sizeof(float));
+    memcpy(&y, buffer + 8, sizeof(float));
+    memcpy(&z, buffer + 12, sizeof(float));
+    memcpy(&theta_w, buffer + 16, sizeof(float));
+    memcpy(&theta_x, buffer + 20, sizeof(float));
+    memcpy(&theta_y, buffer + 24, sizeof(float));
+    memcpy(&theta_z, buffer + 28, sizeof(float));
 
-  std::cout << "Received: "
-            << "x=" << x << ", y=" << y << ", z=" << z << ", theta=" << theta
-            << ", dx=" << dx << ", dy=" << dy << ", dz=" << dz
-            << ", dtheta=" << dtheta << std::endl;
-
-  // Update the SmartDashboard values
-  frc::SmartDashboard::PutNumber("x", x);
-  frc::SmartDashboard::PutNumber("y", y);
-  frc::SmartDashboard::PutNumber("z", z);
-  frc::SmartDashboard::PutNumber("theta", theta);
-  frc::SmartDashboard::PutNumber("dx", dx);
-  frc::SmartDashboard::PutNumber("dy", dy);
-  frc::SmartDashboard::PutNumber("dz", dz);
-  frc::SmartDashboard::PutNumber("dtheta", dtheta);
+    std::cout << "Received: "
+              << "x=" << x << ", y=" << y << ", z=" << z << ", theta_w=" << theta_w
+              << ", theta_x=" << theta_x << ", theta_y=" << theta_y << ", theta_z=" << theta_z
+              << std::endl;
+    // Update the SmartDashboard values
+    frc::SmartDashboard::PutNumber("x", x);
+    frc::SmartDashboard::PutNumber("y", y);
+    frc::SmartDashboard::PutNumber("z", z);
+    frc::SmartDashboard::PutNumber("theta_w", theta_w);
+    frc::SmartDashboard::PutNumber("theta_x", theta_x);
+    frc::SmartDashboard::PutNumber("theta_y", theta_y);
+    frc::SmartDashboard::PutNumber("theta_z", theta_z);
 }
