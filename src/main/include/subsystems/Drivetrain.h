c4a965e361d07fe7a4b7474e0c2a1d470c6ae23f
Ashwin Naren <arihant2math@gmail.com>
2024-03-21 12:34:43 -0700

    I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP I HATE CPP

--- src/main/include/subsystems/Drivetrain.h
+++ src/main/include/subsystems/Drivetrain.h
@@ -16,60 +16,62 @@
 
 class Drivetrain : public frc2::SubsystemBase {
 public:
-  Drivetrain();
-  void Periodic() override;
+    Drivetrain();
 
-  inline const frc::Pose2d &GetPose() const { return m_odometry.GetPose(); }
+    void Periodic() override;
 
-  inline void ResetPose(const frc::Pose2d &pose) {
-    m_odometry.ResetPosition(
-        m_IMU.GetAngle(),
-        {m_frontLeft.GetPosition(), m_frontRight.GetPosition(),
-         m_backLeft.GetPosition(), m_backRight.GetPosition()},
-        pose);
-  }
+    inline const frc::Pose2d &GetPose() const { return m_odometry.GetPose(); }
 
-  inline const frc::ChassisSpeeds GetSpeeds() {
-    return m_kinematics.ToChassisSpeeds(
-        {m_frontLeft.GetState(), m_frontRight.GetState(), m_backLeft.GetState(),
-         m_backRight.GetState()});
-  }
+    inline void ResetPose(const frc::Pose2d &pose) {
+        m_odometry.ResetPosition(
+                m_IMU.GetAngle(),
+                {m_frontLeft.GetPosition(), m_frontRight.GetPosition(),
+                 m_backLeft.GetPosition(), m_backRight.GetPosition()},
+                pose);
+    }
 
-  void DriveRobotRelative(const frc::ChassisSpeeds &robotRelativeSpeeds);
-  inline void
-  driveFieldRelative(const frc::ChassisSpeeds &fieldRelativeSpeeds) {
-    DriveRobotRelative(frc::ChassisSpeeds::FromFieldRelativeSpeeds(
-        fieldRelativeSpeeds, GetPose().Rotation()));
-  }
+    inline const frc::ChassisSpeeds GetSpeeds() {
+        return m_kinematics.ToChassisSpeeds(
+                {m_frontLeft.GetState(), m_frontRight.GetState(), m_backLeft.GetState(),
+                 m_backRight.GetState()});
+    }
 
-  void UpdateOdometry();
+    void DriveRobotRelative(const frc::ChassisSpeeds &robotRelativeSpeeds);
+
+    inline void
+    driveFieldRelative(const frc::ChassisSpeeds &fieldRelativeSpeeds) {
+        DriveRobotRelative(frc::ChassisSpeeds::FromFieldRelativeSpeeds(
+                fieldRelativeSpeeds, GetPose().Rotation()));
+    }
+
+    void UpdateOdometry();
 
 private:
-  frc::Translation2d m_frontLeftLocation{kRobotRadius, kRobotRadius};
-  frc::Translation2d m_frontRightLocation{kRobotRadius, -kRobotRadius};
-  frc::Translation2d m_backLeftLocation{-kRobotRadius, kRobotRadius};
-  frc::Translation2d m_backRightLocation{-kRobotRadius, -kRobotRadius};
+    frc::Translation2d m_frontLeftLocation{kRobotRadius, kRobotRadius};
+    frc::Translation2d m_frontRightLocation{kRobotRadius, -kRobotRadius};
+    frc::Translation2d m_backLeftLocation{-kRobotRadius, kRobotRadius};
+    frc::Translation2d m_backRightLocation{-kRobotRadius, -kRobotRadius};
 
-  SwerveModule m_frontLeft{kFrontLeftTurningMotorID, kFrontLeftDriveMotorID,
-                           kFrontLeftCancoderID, kFrontLeftAngleOffset};
-  SwerveModule m_frontRight{kFrontRightTurningMotorID, kFrontRightDriveMotorID,
-                            kFrontRightCancoderID, kFrontRightAngleOffset};
-  SwerveModule m_backLeft{kBackLeftTurningMotorID, kBackLeftDriveMotorID,
-                          kBackLeftCancoderID, kBackLeftAngleOffset};
-  SwerveModule m_backRight{kBackRightTurningMotorID, kBackRightDriveMotorID,
-                           kBackRightCancoderID, kBackRightAngleOffset};
+    SwerveModule m_frontLeft{kFrontLeftTurningMotorID, kFrontLeftDriveMotorID,
+                             kFrontLeftCancoderID, kFrontLeftAngleOffset};
+    SwerveModule m_frontRight{kFrontRightTurningMotorID, kFrontRightDriveMotorID,
+                              kFrontRightCancoderID, kFrontRightAngleOffset};
+    SwerveModule m_backLeft{kBackLeftTurningMotorID, kBackLeftDriveMotorID,
+                            kBackLeftCancoderID, kBackLeftAngleOffset};
+    SwerveModule m_backRight{kBackRightTurningMotorID, kBackRightDriveMotorID,
+                             kBackRightCancoderID, kBackRightAngleOffset};
 
-  frc::ADIS16470_IMU m_IMU;
+    frc::ADIS16470_IMU m_IMU;
 
-  frc::SwerveDriveKinematics<4> m_kinematics{
-      m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation,
-      m_backRightLocation};
+    frc::SwerveDriveKinematics<4> m_kinematics{
+            m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation,
+            m_backRightLocation};
 
-  frc::SwerveDriveOdometry<4> m_odometry{
-      m_kinematics,
-      m_IMU.GetAngle(),
-      {m_frontLeft.GetPosition(), m_frontRight.GetPosition(),
-       m_backLeft.GetPosition(), m_backRight.GetPosition()}};
+    frc::SwerveDriveOdometry<4> m_odometry{
+            m_kinematics,
+            m_IMU.GetAngle(),
+            {m_frontLeft.GetPosition(), m_frontRight.GetPosition(),
+             m_backLeft.GetPosition(), m_backRight.GetPosition()}};
 
-  frc::Field2d field;
+    frc::Field2d field;
 };
